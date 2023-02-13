65a88a24f0e650fd9be4e71d88d6efe2eaaa930d
whitev4 <whitev4@wwu.edu>
2023-02-13 12:17:55 -0800

    updated V1_models to include generator

--- V1-models/V1_models.py
+++ V1-models/V1_models.py
@@ -175,4 +175,110 @@ class BN_V1_V1_LinearLayer_MNIST(nn.Module):
         
         beta = self.clf(concat) 
         return beta
+    
+class Generator_V1_MNIST(nn.Module):
+    def __init__(self, hidden_dim, output_dim, size, spatial_freq, scale, bias, seed=None):
+        super(Generator_V1_MNIST, self).__init__()
+        self.v1_layer = nn.Conv2d(in_channels=1, out_channels=hidden_dim, kernel_size=7, stride=1, padding=3, 
+                                  bias=bias) 
+        self.v1_layer2 = nn.Conv2d(in_channels=hidden_dim, out_channels=hidden_dim, kernel_size=7, stride=1, padding=3, 
+                                   bias=bias)
+        self.clf = nn.Linear((1 * (8 ** 2)) + (hidden_dim * (8 ** 2)) + (hidden_dim * (8 ** 2)), output_dim)
+        self.relu = nn.ReLU()
+        self.bn = nn.BatchNorm2d(1)
+        self.bn0 = nn.BatchNorm2d(1)
+        self.bn1 = nn.BatchNorm2d(hidden_dim)
+        self.bn2 = nn.BatchNorm2d(hidden_dim)
+        
+        scale1 = 1 / (3 * 7 * 7)
+        scale2 = 1 / (hidden_dim * 7 * 7)
+        center = None
+        
+        V1_init(self.v1_layer, size, spatial_freq, center, scale1, bias, seed)
+        self.v1_layer.weight.requires_grad = False
+        
+        V1_init(self.v1_layer2, size, spatial_freq, center, scale2, bias, seed)
+        self.v1_layer2.weight.requires_grad = False
+        
+        if bias==True:
+            self.v1_layer.bias.requires_grad = False
+            self.v1_layer2.bias.requires_grad = False
+        
+    def forward(self, x):  #[128, 1, 28, 28]
+        h1 = self.relu(self.v1_layer(self.bn(x)))  
+        h2 = self.relu(self.v1_layer2(h1))  
+        
+        pool = nn.AvgPool2d(kernel_size=4, stride=4, padding=2)  
+        x_pool = self.bn0(pool(x))  
+        h1_pool = self.bn1(pool(h1))  
+        h2_pool = self.bn2(pool(h2))  
+        
+        x_flat = x_pool.view(x_pool.size(0), -1)  
+        h1_flat = h1_pool.view(h1_pool.size(0),  -1)  
+        h2_flat = h2_pool.view(h2_pool.size(0), -1) 
+        
+        concat = torch.cat((x_flat, h1_flat, h2_flat), 1)  
+        
+        beta = self.clf(concat) 
+        return beta
+
+class Generator_V1_celeba(nn.Module):
+    def __init__(self, hidden_dim, output_dim, size, spatial_freq, scale, bias, seed=None):
+        super(Generator_V1_celeba, self).__init__()
+        self.v1_layer = nn.Conv2d(in_channels=3, out_channels=hidden_dim, kernel_size=7, stride=1, padding=3, 
+                                  bias=bias) 
+        self.v1_layer2 = nn.Conv2d(in_channels=hidden_dim, out_channels=hidden_dim, kernel_size=7, stride=1, padding=3, 
+                                   bias=bias)
+        self.clf = nn.Linear(221067, output_dim)
+        self.relu = nn.ReLU()
+        self.bn = nn.BatchNorm2d(3)
+        self.bn0 = nn.BatchNorm2d(3)
+        self.bn1 = nn.BatchNorm2d(hidden_dim)
+        self.bn2 = nn.BatchNorm2d(hidden_dim)
+        
+        scale1 = 1 / (3 * 7 * 7)
+        scale2 = 1 / (hidden_dim * 7 * 7)
+        center = None
+        
+        V1_init(self.v1_layer, size, spatial_freq, center, scale1, bias, seed)
+        self.v1_layer.weight.requires_grad = False
+        
+        V1_init(self.v1_layer2, size, spatial_freq, center, scale2, bias, seed)
+        self.v1_layer2.weight.requires_grad = False
+        
+        if bias==True:
+            self.v1_layer.bias.requires_grad = False
+            self.v1_layer2.bias.requires_grad = False
+        
+    def forward(self, x):  #[2, 3, 218, 178]
+        #print("x: ", x.shape)
+        #print("bn: ", self.bn(x).shape)
+        h1 = self.relu(self.v1_layer(self.bn(x)))  
+        h2 = self.relu(self.v1_layer2(h1))  
+        
+        #print("h1: ", h1.shape)
+        #print("h2: ", h2.shape)
+        
+        pool = nn.AvgPool2d(kernel_size=4, stride=4, padding=2)  
+        x_pool = self.bn0(pool(x))  
+        h1_pool = self.bn1(pool(h1))  
+        h2_pool = self.bn2(pool(h2))  
+
+        
+        #print("x pool ", x_pool.shape)
+        #print("h1_pool: ", h1_pool.shape)
+        #print("h2 pool: ", h2_pool.shape)
+        
+        x_flat = x_pool.view(x_pool.size(0), -1)  
+        h1_flat = h1_pool.view(h1_pool.size(0),  -1)  
+        h2_flat = h2_pool.view(h2_pool.size(0), -1) 
+        
+        #print("x flat: ", x_flat.shape)
+        #print("h1 flat: ", h1_flat.shape)
+        #print("h2 flat: ", h2_flat.shape)
+        
+        concat = torch.cat((x_flat, h1_flat, h2_flat), 1)  
+        
+        beta = self.clf(concat) 
+        return beta
 
