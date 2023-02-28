d48565845ffeea26829bba73e286415665f6a511
whitev4 <whitev4@wwu.edu>
2023-02-28 15:22:22 -0800

    working on celeba generator

--- V1-models/whitening_incremental_V1_celeba.py
+++ V1-models/whitening_incremental_V1_celeba.py
@@ -26,7 +26,7 @@ from torch.utils.data import Subset
 device = "cuda:1" if torch.cuda.is_available() else "cpu"
 
 class Generator(nn.Module):
-    def __init__(self, num_input_channels, num_hidden_channels, num_output_channels=3, filter_size=3): #does filter size change anything
+    def __init__(self, num_input_channels, num_hidden_channels, num_output_channels=3, filter_size=7): #does filter size change anything
         super(Generator, self).__init__()
         self.num_input_channels = num_input_channels
         self.num_hidden_channels = num_hidden_channels
@@ -36,12 +36,26 @@ class Generator(nn.Module):
 
     def build(self):
         # padding = (self.filter_size - 1) // 2
-        padding = 1
+        padding = 3
         self.main = nn.Sequential(
-            nn.Linear(self.num_input_channels, self.num_hidden_channels * 32 * 32),
-            nn.Unflatten(1, (self.num_hidden_channels, 32, 32)), #changing these - image dim / 4
+            nn.Linear(self.num_input_channels, self.num_hidden_channels * 4 * 4),
+            nn.Unflatten(1, (self.num_hidden_channels, 4, 4)), #changing these - image dim / 4 -- WHY
             nn.ReLU(inplace=True),
        
+            nn.ReflectionPad2d(padding),
+            nn.Conv2d(self.num_hidden_channels, self.num_hidden_channels, self.filter_size, bias=False),
+            nn.BatchNorm2d(self.num_hidden_channels, eps=0.001, momentum=0.9),
+            nn.ReLU(inplace=True),
+            nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False),
+            
+            # added
+            nn.ReflectionPad2d(padding),
+            nn.Conv2d(self.num_hidden_channels, self.num_hidden_channels, self.filter_size, bias=False),
+            nn.BatchNorm2d(self.num_hidden_channels, eps=0.001, momentum=0.9),
+            nn.ReLU(inplace=True),
+            nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False),
+            
+            # added
             nn.ReflectionPad2d(padding),
             nn.Conv2d(self.num_hidden_channels, self.num_hidden_channels, self.filter_size, bias=False),
             nn.BatchNorm2d(self.num_hidden_channels, eps=0.001, momentum=0.9),
@@ -88,47 +102,41 @@ if __name__ == '__main__':
 
     transforms_to_apply = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Resize((128,128)),
+        transforms.Resize((64,64)),
         transforms.Normalize((0.5,), (0.5,))  # Normalization for reproducibility issues
     ])
     
-    #mnist_dir = get_dataset_dir("MNIST", create=True)
-    #dataset = datasets.MNIST(mnist_dir, train=True, download=True, transform=transforms_to_apply)
-    
-    #celeba_dir = get_dataset_dir("CelebA/celeba", create=False)
     root = "/research/harris/vivian/v1-models/datasets/"
-    dataset = datasets.CelebA(root=root, split="train", download=False, transform=transforms_to_apply)
-        
-    #train_subset = np.random.choice(np.array(range(len(dataset))), replace=False, size=20000) #65536
-    #datasubset = Subset(dataset, train_subset)
-    
-    #dataloader = DataLoader(datasubset, batch_size=128, shuffle=False, pin_memory=True, drop_last=True)
+    train_dataset = datasets.CelebA(root=root, split="train", download=False, transform=transforms_to_apply)
+    train_dataloader = DataLoader(train_dataset, batch_size=128, shuffle=False, pin_memory=True, drop_last=True) 
 
-    dataloader = DataLoader(dataset, batch_size=128, shuffle=False, pin_memory=True, drop_last=True) 
+    fixed_dataloader_train = DataLoader(train_dataset, batch_size=2, shuffle=False)
+    fixed_batch_train = next(iter(fixed_dataloader_train))
+    fixed_batch_train = fixed_batch_train[0].float().to(device)
 
-    fixed_dataloader = DataLoader(dataset, batch_size=2, shuffle=False)
-    fixed_batch = next(iter(fixed_dataloader))
-    fixed_batch = fixed_batch[0].float().to(device)
-
-    scattering = V1_models.Generator_V1_celeba(num_input_channels, 100, 2, 0.1, 1, True).to(device)
+    scattering = V1_models.Generator_V1_celeba(num_input_channels, 128, 2, 0.1, 1, True).to(device)
     scattering.requires_grad = False
-    scattering_fixed_batch = scattering(fixed_batch).squeeze(1) #2, 81, 7, 7
+    scattering_fixed_batch_train = scattering(fixed_batch_train).squeeze(1) #2, 81, 7, 7
+    
+    test_dataset = datasets.CelebA(root=root, split="test", download=False, transform=transforms_to_apply)
+    test_dataloader = DataLoader(test_dataset, batch_size=128, shuffle=False, pin_memory=True, drop_last=True) 
+
+    fixed_dataloader_test = DataLoader(test_dataset, batch_size=2, shuffle=False)
+    fixed_batch_test = next(iter(fixed_dataloader_test))
+    fixed_batch_test = fixed_batch_test[0].float().to(device)
+    scattering_fixed_batch_test = scattering(fixed_batch_test).squeeze(1)
     
     whitener = IncrementalPCA(n_components=num_input_channels, whiten=True)
     
     for idx_epoch in range(1): #2 epochs
         print('Whitening training epoch {}'.format(idx_epoch))
-        for idx, batch in enumerate(dataloader): #469 batches
+        for idx, batch in enumerate(train_dataloader): #469 batches
             images = batch[0].float().to(device)
             batch_scatter = scattering(images).view(images.size(0), -1).cpu().detach().numpy()
             whitener.partial_fit(batch_scatter)
     print("Done whitening")
-    #kymatio = Scattering(J=2, shape=(128,128)).to(device)
-    #kymatio_fixed_batch = kymatio(fixed_batch).squeeze(1)
-    #input_channels = kymatio_fixed_batch.shape[1]
     
     
-    #print("num input channels kymatio: " + str(input_channels))
     num_hidden_channels = 128
 
     generator = Generator(num_input_channels, num_hidden_channels).to(device)
@@ -147,7 +155,7 @@ if __name__ == '__main__':
 
         for idx_epoch in range(num_epochs):
             print('Generator training epoch {}'.format(idx_epoch))
-            for _, current_batch in enumerate(dataloader):
+            for _, current_batch in enumerate(train_dataloader):
                 generator.zero_grad()
                 batch_images = Variable(current_batch[0]).float().to(device) #[128, 3, 128, 128]
                 batch_scattering = scattering(batch_images).view(batch_images.size(0), -1).cpu().detach().numpy() #[128, 100]
@@ -171,12 +179,22 @@ if __name__ == '__main__':
    
     # We create the batch containing the linear interpolation points in the scattering space
     ########################################################################################
-        
-        
-    z0 = scattering_fixed_batch.cpu().detach().numpy()[[0]]
-    z1 = scattering_fixed_batch.cpu().detach().numpy()[[1]]
+   # fixed_dataloader = DataLoader(dataset, 2, shuffle=True)
+    #fixed_batch = next(iter(fixed_dataloader))
 
-    batch_z = np.copy(z0)
+   # z0 = fixed_batch['z'][[0]].numpy()
+   # z1 = fixed_batch['z'][[1]].numpy()
+#
+   # batch_z = np.copy(z0)
+        ####
+    z0 = scattering_fixed_batch_train.cpu().detach().numpy()[[0]]
+    z1 = scattering_fixed_batch_train.cpu().detach().numpy()[[1]]
+    
+    z2 = scattering_fixed_batch_test.cpu().detach().numpy()[[0]]
+    z3 = scattering_fixed_batch_test.cpu().detach().numpy()[[1]]
+    
+    batch_z_train = np.copy(z0)
+    batch_z_test = np.copy(z2)
 
     nb_samples = 32
 
@@ -184,39 +202,70 @@ if __name__ == '__main__':
     for t in interval:
         if t > 0:      
             zt = (1 - t) * z0 + t * z1
-            batch_z = np.vstack((batch_z, zt))
+            batch_z_train = np.vstack((batch_z_train, zt))
 
-    z = torch.from_numpy(batch_z).float().to(device)
+    z = torch.from_numpy(batch_z_train).float().to(device)
     g_z = generator.forward(z)
     g_z = g_z.data.cpu().numpy().transpose((0, 2, 3, 1))
 
 
     for idx in range(nb_samples):
-        filename_image = os.path.join(dir_to_save, '{}.png'.format(idx))
+        filename_image = os.path.join(dir_to_save, '{}_train.png'.format(idx))
         Image.fromarray(np.uint8((g_z[idx] + 1) * 127.5)).save(filename_image)   
         
-   
+    for t in interval:
+        if t > 0:      
+            zt = (1 - t) * z2 + t * z3
+            batch_z_test = np.vstack((batch_z_test, zt))
+
+    z = torch.from_numpy(batch_z_test).float().to(device)
+    g_z = generator.forward(z)
+    g_z = g_z.data.cpu().numpy().transpose((0, 2, 3, 1))
+
+
+    for idx in range(nb_samples):
+        filename_image = os.path.join(dir_to_save, '{}_test.png'.format(idx))
+        Image.fromarray(np.uint8((g_z[idx] + 1) * 127.5)).save(filename_image)   
+
+
     # code for generating 16 random images
     #####################################
     nb_samples = 16
     z = np.random.randn(nb_samples, num_input_channels)
     z = torch.from_numpy(z).float().to(device)
     g_z = generator.forward(z)
-    filename_images = os.path.join(dir_to_save, 'epoch_random.png')
+    filename_images = os.path.join(dir_to_save, 'train_random.png')
     temp = make_grid(g_z.data[:16], nrow=4).cpu().numpy().transpose((1, 2, 0))
     Image.fromarray(np.uint8((temp + 1) * 127.5)).save(filename_images)
     
     
     
-    #is this the code for reconstruction?
-    print("maybe reconstructing")
-    fixed_dataloader = DataLoader(dataset, 16)
-    fixed_batch = next(iter(fixed_dataloader))
-
-    #z = fixed_batch['z'].float().cuda()
-    z = fixed_batch.cpu().detach().numpy()
+    #code for reconstructing from train/test sets
+    ####################################
+    fixed_dataloader_train = DataLoader(train_dataset, 16)
+    fixed_batch_train = next(iter(fixed_dataloader_train))
+    
+    #z = np.array(fixed_batch_train)
+    #z = torch.from_numpy(np.array(fixed_batch_train)).float().to(device)
+    
+    
+    z = fixed_batch_train.float().to(device)
+    #list object has to attribute float
+    #try fixed_batch_train.to(device)
+    
     g_z = generator.forward(z)
     filename_images = os.path.join(dir_to_save, 'train_reconstruct.png')
+    temp = make_grid(g_z.data[:16], nrow=4).cpu().numpy().transpose((1, 2, 0))
+    Image.fromarray(np.uint8((temp + 1) * 127.5)).save(filename_images)
+    
+    fixed_dataloader_test = DataLoader(test_dataset, 16)
+    fixed_batch_test = next(iter(fixed_dataloader_test))
+    
+    
+    z = fixed_batch_test.float().to(device)
+  
+    g_z = generator.forward(z)
+    filename_images = os.path.join(dir_to_save, 'test_reconstruct.png')
     temp = make_grid(g_z.data[:16], nrow=4).cpu().numpy().transpose((1, 2, 0))
     Image.fromarray(np.uint8((temp + 1) * 127.5)).save(filename_images)
     
