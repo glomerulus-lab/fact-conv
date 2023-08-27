94a0d04b5d6044df6a0f01cc4052ffde883c2c92
Kameron Harris <kamerondeckerharris@gmail.com>
2023-08-26 17:50:52 -0700

    learnable

---
+++ V1-models/LearnableCov.py
@@ -0,0 +1,102 @@
+import torch
+import torch.nn as nn
+from torch.nn.parameter import Parameter, UninitializedParameter
+from nn.common_types import _size_2_t
+from typing import Optional, List, Tuple, Union
+
+class LearnableLinearCov(nn.Module):
+    in_features: int
+    out_features: int
+    weight: torch.Tensor
+
+    def __init__(self, in_features: int, out_features: int, bias: bool = True,
+                 device=None, dtype=None) -> None:
+        factory_kwargs = {'device': device, 'dtype': dtype}
+        super().__init__()
+        
+        self.in_features = in_features
+        self.out_features = out_features
+        self.factory_kwargs = factory_kwargs
+
+        self.register_buffer("weight",
+                             torch.randn((out_features, in_features),
+                                         **factory_kwargs))
+
+        self.triu_len = torch.triu_indices(in_features, in_features).shape[1]
+        self.tri_vec = Parameter(torch.empty((triu_len,), **factory_kwargs))
+        if bias:
+            self.bias = Parameter(torch.empty(out_features, **factory_kwargs))
+        else:
+            self.register_parameter('bias', None)
+        self.reset_parameters()
+
+    def reset_parameters(self) -> None:
+        nn.init.constant_(self.tri_vec, 0.)
+        # Setting a=sqrt(5) in kaiming_uniform is the same as initializing with
+        # uniform(-1/sqrt(in_features), 1/sqrt(in_features)). For details, see
+        # https://github.com/pytorch/pytorch/issues/57109
+        nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        if self.bias is not None:
+            fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weight)
+            bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
+            nn.init.uniform_(self.bias, -bound, bound)
+
+    def forward(self, input: Tensor) -> Tensor:
+        U = torch.zeros((self.in_features, self.in_features),
+                        **self.factory_kwargs)
+        U[torch.triu_indices(self.in_features, self.in_features).tolist()] \
+            = self.tri_vec
+        exp_diag = torch.exp(torch.diagonal(U))
+        U[range(self.in_features), range(self.in_features)] = exp_diag
+        composite_weight = self.weight @ U
+        
+        return F.linear(input, composite_weight, self.bias)
+
+class LearnableCovConv2d(nn.Conv2d):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: _size_2_t,
+        stride: _size_2_t = 1,
+        padding: Union[str, _size_2_t] = 0,
+        dilation: _size_2_t = 1,
+        groups: int = 1,
+        bias: bool = True,
+        padding_mode: str = 'zeros',  # TODO: refine this type
+        device=None,
+        dtype=None
+    ) -> None:
+        # init as Conv2d
+        super().__init__(
+            in_channels, out_channels, kernel_size, stride, padding, dilation, 
+            groups, bias, padding_mode, device, dtype)
+
+        factory_kwargs = {'device': device, 'dtype': dtype}
+        self.factory_kwargs = factory_kwargs
+
+        # weight shape: (out_channels, in_channels // groups, *kernel_size)
+        self.weight = torch.randn(self.weight.shape, **factory_kwargs,
+                                  requires_grad=False)
+        
+        self.in_features = self.in_channels // self.groups * \
+            self.kernel_size[0] * self.kernel_size[1]
+        self.triu_len = torch.triu_indices(self.in_features,
+                                           self.in_features).shape[1]
+        self.tri_vec = Parameter(torch.zeros((triu_len,), **factory_kwargs))
+        
+    def forward(self, input: Tensor) -> Tensor:
+        U = torch.zeros((self.in_features, self.in_features),
+                        **self.factory_kwargs)
+        U[torch.triu_indices(self.in_features, self.in_features).tolist()] \
+            = self.tri_vec
+        exp_diag = torch.exp(torch.diagonal(U))
+        U[range(self.in_features), range(self.in_features)] = exp_diag
+        
+        matrix_shape = (out_channels, self.in_features)
+        composite_weight = torch.reshape(
+            torch.reshape(self.weight, matrix_shape) @ U,
+            self.weight.shape
+        )
+        
+        return self._conv_forward(input, composite_weight, self.bias)
