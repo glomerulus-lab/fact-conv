6c44762009802c57c50a05cf58696c347ad29dae
Kameron Harris <kamerondeckerharris@gmail.com>
2023-11-13 22:35:33 -0800

    cifar10 small samps

--- V1-models/BN_V1_V1_Linear_CIFAR10_Small_Sample.py
+++ V1-models/BN_V1_V1_Linear_CIFAR10_Small_Sample.py
@@ -11,6 +11,7 @@ import argparse
 from numpy.random import RandomState
 import numpy as np
 import V1_models
+import LC_models
 
 def save_model(args, model, loss, accuracy):
     src = "/research/harris/vivian/v1-models/saved-models/CIFAR10_50_Samples/"
@@ -60,8 +61,10 @@ if __name__ == '__main__':
     device = torch.device("cuda:" + str(args.device) if use_cuda else "cpu")
     
     start = datetime.now()
-    model = V1_models.Learned_Rand_Scat_CIFAR10(args.hidden_dim, args.s, args.f, args.scale, args.bias).to(device)
+    #model = V1_models.Learned_Rand_Scat_CIFAR10(args.hidden_dim, args.s, args.f, args.scale, args.bias).to(device)
     #model = V1_models.BN_V1_V1_LinearLayer_CIFAR10(args.hidden_dim, args.s, args.f, args.scale, args.bias).to(device)
+    model = LC_models.BN_V1_V1_LinearLayer_CIFAR10(args.hidden_dim, args.s, args.f, args.scale, args.bias).to(device)
+
     # DataLoaders
     if use_cuda:
         num_workers = 4
