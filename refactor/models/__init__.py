a779085803f75fde9635b962415d3f1519e6c721
vivianwhite <vivcat980@gmail.com>
2024-04-21 18:39:39 -0400

    fixing V1 cifar10 model

--- refactor/models/__init__.py
+++ refactor/models/__init__.py
@@ -1,1 +1,12 @@
 from .resnet import ResNet18
+from .V1_models import V1_CIFAR10, V1_CIFAR100
+
+def define_models(args):
+    if args.net == 'resnet18':
+        model = ResNet18()
+    elif args.net == 'rsn_cifar10':
+        model = V1_CIFAR10(hidden_dim=100, size=args.s,
+                spatial_freq=args.f, scale=args.scale)
+    elif args.net == 'rsn_cifar100':
+        model = V1_CIFAR100()
+    return model
