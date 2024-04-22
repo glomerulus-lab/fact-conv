b8732541f64bcce6db896a97a8fdcc7ed99e572d
vivianwhite <vivcat980@gmail.com>
2024-04-22 01:03:16 -0400

    add replace_linears function

--- refactor/models/__init__.py
+++ refactor/models/__init__.py
@@ -1,5 +1,5 @@
 from .resnet import ResNet18
-from .V1_models import V1_CIFAR10, V1_CIFAR100
+from .V1_models import V1_CIFAR10, V1_CIFAR100, replace_linear_layer
 
 def define_models(args):
     if args.net == 'resnet18':
@@ -8,5 +8,7 @@ def define_models(args):
         model = V1_CIFAR10(hidden_dim=100, size=args.s,
                 spatial_freq=args.f, scale=args.scale)
     elif args.net == 'rsn_cifar100':
-        model = V1_CIFAR100()
+        model = V1_CIFAR10(hidden_dim=100, size=args.s,
+                spatial_freq=args.f, scale=args.scale)
+        replace_linear_layer(model)
     return model
