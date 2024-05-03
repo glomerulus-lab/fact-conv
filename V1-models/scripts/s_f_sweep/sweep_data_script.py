428ae583ceaff47228ddf19ada3013ab7f841d64
vivianwhite <66977221+vivianwhite@users.noreply.github.com>
2024-05-03 15:49:08 -0700

    Delete V1-models/scripts/s_f_sweep directory

--- V1-models/scripts/s_f_sweep/sweep_data_script.py
+++
@@ -1,6 +0,0 @@
-f_list = [0.1, 2.0]
-s_list = [1, 2, 3]
-for f in f_list:
-    for s in s_list:
-        new_name = "s_"+str(s)+"_f_"+str(f)
-        print("python3 sweep_data.py --s={} --f={} ".format(s, f))
