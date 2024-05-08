aefc771689f8a32464cf7d98acec1fa2edd1ccbd
vivianwhite <vivcat980@gmail.com>
2024-05-08 08:42:20 -0400

    conv_modules.py

--- FactConv/models/__init__.py
+++ FactConv/models/__init__.py
@@ -1,6 +1,6 @@
 from .resnet import ResNet18
 from .function_utils import replace_layers_factconv2d,\
-replace_layers_diagfactconv2d, turn_off_covar_grad, replace_layers_scale, init_V1_layers
+replace_layers_diagfactconv2d, replace_layers_diagchanfactconv2d, turn_off_covar_grad, replace_layers_scale, init_V1_layers
 
 
 def define_models(args):
@@ -12,6 +12,8 @@ def define_models(args):
        replace_layers_factconv2d(model)
     if 'diag' in args.net:
         replace_layers_diagfactconv2d(model)
+    if 'diagchan' in args.net:
+        replace_layers_diagchanfactconv2d(model)
     if "v1" in args.net:
         init_V1_layers(model, bias=False)
     if "us" in args.net:
