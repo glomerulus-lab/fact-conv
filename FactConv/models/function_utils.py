74aa5a3ec40dc14d7cb3b63f64ab2e52765c0823
vivianwhite <vivcat980@gmail.com>
2024-05-08 08:42:20 -0400

    conv_modules.py

--- FactConv/models/function_utils.py
+++ FactConv/models/function_utils.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from conv_modules import FactConv2d, DiagFactConv2d
+from conv_modules import FactConv2d, DiagFactConv2d, DiagChanFactConv2d
 from V1_covariance import V1_init
 
 
@@ -59,6 +59,28 @@ def replace_layers_diagfactconv2d(model):
             new_module.load_state_dict(new_sd)
             return new_module
     return recurse_preorder(model, _replace_layers_diagfactconv2d)
+
+def replace_layers_diagchanfactconv2d(model):
+    '''
+    Replace nn.Conv2d layers with DiagChanFactConv2d
+    '''
+    def _replace_layers_diagchanfactconv2d(module):
+        if isinstance(module, nn.Conv2d):
+            ## simple module
+            new_module = DiagChanFactConv2d(
+                    in_channels=module.in_channels,
+                    out_channels=module.out_channels,
+                    kernel_size=module.kernel_size,
+                    stride=module.stride, padding=module.padding, 
+                    bias=True if module.bias is not None else False)
+            old_sd = module.state_dict()
+            new_sd = new_module.state_dict()
+            new_sd['weight'] = old_sd['weight']
+            if module.bias is not None:
+                new_sd['bias'] = old_sd['bias']
+            new_module.load_state_dict(new_sd)
+            return new_module
+    return recurse_preorder(model, _replace_layers_diagchanfactconv2d)
 
 
 def replace_affines(model):
