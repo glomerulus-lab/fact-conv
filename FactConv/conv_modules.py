a8c7b5d2f8c933f25dffe782303140316b12553e
vivianwhite <vivcat980@gmail.com>
2024-06-28 15:47:03 -0400

    adding learnable low-rank diagonal covariance

--- FactConv/conv_modules.py
+++ FactConv/conv_modules.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 from torch.nn.parameter import Parameter
 from torch.nn.common_types import _size_2_t
 from typing import Optional, List, Tuple, Union
-from cov import Covariance, LowRankCovariance
+from cov import Covariance, LowRankCovariance, LowRankPlusDiagCovariance
 
 """ 
 The function below is copied directly from 
@@ -52,10 +52,8 @@ class FactConv2d(nn.Conv2d):
         self.spatial = Covariance(spatial_triu_size)
 
     def forward(self, input: Tensor) -> Tensor:
-        U1 = self.channel._tri_vec_to_mat(self.channel.tri_vec,
-                self.channel.triu_size, self.channel.scat_idx)
-        U2 = self.spatial._tri_vec_to_mat(self.spatial.tri_vec,
-                self.spatial.triu_size, self.spatial.scat_idx)
+        U1 = self.channel.sqrt()
+        U2 = self.spatial.sqrt()
 
         # flatten over filter dims and contract
         composite_weight = _contract(self.weight, U1.T, 1)
@@ -98,14 +96,61 @@ class LowRankFactConv2d(nn.Conv2d):
         spatial_triu_size = self.kernel_size[0] * self.kernel_size[1]
 
         self.channel = LowRankCovariance(channel_triu_size, self.channel_k)
-        self.spatial = LowRankCovariance(spatial_triu_size, self.spatial_k)
+        #self.spatial = LowRankCovariance(spatial_triu_size, self.spatial_k)
+        self.spatial = Covariance(spatial_triu_size)
 
 
     def forward(self, input: Tensor) -> Tensor:
-        U1 = self.channel._tri_vec_to_mat(self.channel.tri_vec,
-                self.channel.triu_size, self.channel.scat_idx)
-        U2 = self.spatial._tri_vec_to_mat(self.spatial.tri_vec,
-                self.spatial.triu_size, self.spatial.scat_idx)
+        U1 = self.channel.sqrt()
+        U2 = self.spatial.sqrt()
+
+        # flatten over filter dims and contract
+        composite_weight = _contract(self.weight, U1.T, 1)
+        composite_weight = _contract(
+            torch.flatten(composite_weight, -2, -1), U2.T, -1
+        ).reshape(self.weight.shape)
+        return self._conv_forward(input, composite_weight, self.bias)
+
+class LowRankPlusDiagFactConv2d(nn.Conv2d):
+    def __init__(
+        self,
+        spatial_k,
+        channel_k,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: _size_2_t,
+        stride: _size_2_t = 1,
+        padding: Union[str, _size_2_t] = 0,
+        dilation: _size_2_t = 1,
+        groups: int = 1,
+        bias: bool = True,
+        padding_mode: str = 'zeros',  # TODO: refine this type
+        device=None,
+        dtype=None,
+    ) -> None:
+        # init as Conv2d
+        super().__init__(
+            in_channels, out_channels, kernel_size, stride, padding, dilation, 
+            groups, bias, padding_mode, device, dtype)
+        self.spatial_k = spatial_k
+        self.channel_k = channel_k
+        # weight shape: (out_channels, in_channels // groups, *kernel_size)
+        new_weight = torch.empty_like(self.weight)
+        del self.weight # remove Parameter, create buffer
+        self.register_buffer("weight", new_weight)
+        nn.init.kaiming_normal_(self.weight)
+        
+        channel_triu_size = self.in_channels // self.groups
+        spatial_triu_size = self.kernel_size[0] * self.kernel_size[1]
+
+        self.channel = LowRankPlusDiagCovariance(channel_triu_size, self.channel_k)
+        # self.spatial = LowRankCovariance(spatial_triu_size, self.spatial_k)
+        self.spatial = Covariance(spatial_triu_size)
+
+
+    def forward(self, input: Tensor) -> Tensor:
+        U1 = self.channel.sqrt()
+        U2 = self.spatial.sqrt()
         
         # flatten over filter dims and contract
         composite_weight = _contract(self.weight, U1.T, 1)
--- FactConv/cov.py
+++ FactConv/cov.py
@@ -7,12 +7,14 @@ from typing import Optional, List, Tuple, Union
 import math
 
 class Covariance(nn.Module):
+    """ Module for learnable weight covariance matrices 
+        Input: the size of the covariance matrix
+        For channel covariances, triu_size is in_channels // groups
+        For spatial covariances, triu_size is kernel_size[0] * kernel_size[1]
+    """
     def __init__(self, triu_size):
         super().__init__()
         self.triu_size = triu_size
-
-        # for channel cov, triu_size is in_channels // groups
-        # for spatial cov, triu_size is kernel_size[0] * kernel_size[1]
 
         triu = torch.triu_indices(self.triu_size, self.triu_size,
                 dtype=torch.long)
@@ -20,32 +22,61 @@ class Covariance(nn.Module):
         self.register_buffer("scat_idx", scat_idx, persistent=False)
 
         triu_len = triu.shape[1]
-        #tri_vec = self.weight.new_zeros((triu_len,))
         tri_vec = torch.zeros((triu_len,))
         self.tri_vec = Parameter(tri_vec)
 
 
-    def cov(self, R):
+    def cov(self):
+        R = self._tri_vec_to_mat(self.tri_vec, self.triu_size, self.scat_idx)
         return R.T @ R
 
-    def sqrt(self, R):
+    def sqrt(self):
+        R = self._tri_vec_to_mat(self.tri_vec, self.triu_size, self.scat_idx)
         return R
 
     def _tri_vec_to_mat(self, vec, n, scat_idx):
-        #r = self.weight.new_zeros((n*n)).scatter_(0, scat_idx, vec).view(n, n)
         r = torch.zeros((n*n), device=self.tri_vec.device,
                 dtype=self.tri_vec.dtype).scatter_(0, scat_idx, vec).view(n, n)
         r = torch.diagonal_scatter(r, r.diagonal().exp_())
         return r
 
 class LowRankCovariance(Covariance):
+    """ Module for learnable low-rank covariance matrices
+        Input: the size of the covariance matrix and the rank as a percentage
+    """
     def __init__(self, triu_size, rank):
         super().__init__(triu_size)
-        self.k = math.ceil(rank * triu_size)
-#        print("Rank: ", self.k)
+        if triu_size == 3:
+            self.k = 3
+            print(self.k)
+        else:
+            self.k = math.ceil(rank * triu_size)
+        print(self.k)
+
         triu = torch.triu_indices(self.triu_size, self.triu_size,
                 dtype=torch.long)
         mask = triu[0] < self.k
+        scat_idx = triu[0][mask] * self.triu_size + triu[1][mask]
+        self.register_buffer("scat_idx", scat_idx, persistent=False)
+
+        triu_len = scat_idx.shape[0]
+        tri_vec = torch.zeros((triu_len,))
+        self.tri_vec = Parameter(tri_vec)
+
+class LowRankPlusDiagCovariance(Covariance):
+    """ Module for learning low-rank covariances plus the main diagonal
+    input: size of covariance matrix and the percentage rank (rows kept in upper triangular covaraince)"""
+    def __init__(self, triu_size, rank):
+        super().__init__(triu_size)
+        if triu_size == 3:
+            self.k = 3
+            print(self.k)
+        else:
+            self.k = math.ceil(rank * triu_size)
+            print(self.k)
+        triu = torch.triu_indices(self.triu_size, self.triu_size,
+                dtype=torch.long)
+        mask = (triu[0] < self.k)|(triu[0]==triu[1])
         scat_idx = triu[0][mask] * self.triu_size + triu[1][mask]
         self.register_buffer("scat_idx", scat_idx, persistent=False)
 
