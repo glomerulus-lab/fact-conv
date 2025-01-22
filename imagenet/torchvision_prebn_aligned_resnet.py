30744384964ddf2e9873224ec84cee99c007b1ea
vivianwhite <vivcat980@gmail.com>
2025-01-21 23:00:33 -0500

    starting aligned prebn torchvision resnet

--- imagenet/torchvision_prebn_aligned_resnet.py
+++ imagenet/torchvision_prebn_aligned_resnet.py
@@ -4,6 +4,7 @@ from typing import Any, Callable, List, Optional, Type, Union
 import torch
 import torch.nn as nn
 from torch import Tensor
+from align import Alignment
 
 def conv3x3(in_planes: int, out_planes: int, stride: int = 1, groups: int = 1, dilation: int = 1) -> nn.Conv2d:
     """3x3 convolution with padding"""
